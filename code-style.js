1de94ad7b649438c5ac97cc9d01c7b99a7793661
Viktor Varland <viktor@vardevs.se>
2018-12-06 23:16:12 +0100

    fix: move cwd() function into the cmd function for portability

--- code-style.js
+++ code-style.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-// built-ins
-const path = require('path')
-const fs = require('fs')
-
 /**
  * `repoDir`   - points to the repo which needs to be formatted
  * `dir`       - points to the code-style package in node_modules, and
@@ -13,23 +9,7 @@ const fs = require('fs')
 const argv = require('yargs')
     .config({
         root_dir: process.cwd(),
-        cwd: cwd(),
     })
     .usage('usage: $0 <command> [options]')
     .command(require('./lib/cmds/fmt-code.js'))
     .help().argv
-
-function cwd() {
-    try {
-        const nodeModulePath = path.join(
-            process.cwd(),
-            'node_modules',
-            '@dhis2',
-            'code-style'
-        )
-        fs.accessSync(nodeModulePath)
-        return nodeModulePath
-    } catch (err) {
-        return process.cwd()
-    }
-}
