5d4169de55e5cb0b2e24919914e77ac0f5c60678
Viktor Varland <viktor@vardevs.se>
2019-02-06 15:21:07 +0100

    refactor: rename source folder to conventional

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { namespace } = require('@dhis2/cli-helpers-engine')
 const command = namespace('style', {
     desc: 'DHIS2 programmatic style for commit msgs/code',
     aliases: 's',
-    builder: require('./lib/cmds'),
+    builder: require('./src/cmds'),
 })
 
 module.exports = command
