ece8c535c96651c43e71b5f6308db953f1b29b0b
Viktor Varland <viktor@vardevs.se>
2019-03-05 18:57:25 +0100

    fix: clean up js filter mechanism (#21)

--- src/cmds/js_cmds/check.js
+++ src/cmds/js_cmds/check.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { collectJsFiles, whitelists, whitelisted } = require('../../files.js')
+const { collectFiles, jsFiles } = require('../../files.js')
 const log = require('@dhis2/cli-helpers-engine').reporter
 
 const { check_fmt } = require('../../prettier.js')
@@ -24,19 +24,18 @@ exports.handler = argv => {
 
     let codeFiles
     if (all) {
-        codeFiles = collectJsFiles(root_dir)
+        codeFiles = collectFiles(root_dir)
     } else if (files) {
         codeFiles = files
     } else {
-        const whitelist = whitelisted(whitelists.js)
-        codeFiles = staged_files(root_dir).filter(whitelist)
+        codeFiles = staged_files(root_dir)
     }
 
     // debug information about the folders
     log.debug('rootDir?', root_dir)
     log.debug('codeFiles?', codeFiles)
 
-    const prettyFiles = check_fmt(codeFiles)
+    const prettyFiles = check_fmt(jsFiles(codeFiles))
 
     const success = prettyFiles.length > 0
 
