f91ca7d63b11acadb7585b03d3355a0bf9a3777a
Viktor Varland <varl@vlv.io>
2019-07-15 12:52:06 +0200

    refactor: export command from module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const { configObject } = require('./src/groups.js')
+const command = require('./src/index.js')
 
 exports.config = configObject()
+exports.command = command
---
+++ tests/main.js
@@ -0,0 +1,10 @@
+const test = require('tape')
+
+const { command, config } = require('../index.js')
+
+test('base exports are objects', t => {
+    t.plan(2)
+
+    t.ok(typeof command === 'object')
+    t.ok(typeof config === 'object')
+})
