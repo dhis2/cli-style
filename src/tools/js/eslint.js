a89faabe1f38ef7f0b858ab0b48f9c6cab004176
Viktor Varland <viktor@dhis2.org>
2019-07-17 10:00:37 +0200

    fix: allow ignore files to be read (#100)
    * fix: allow ignore files to be read
    
    * fix: skip general messages reported without line numbers

--- src/tools/js/eslint.js
+++ src/tools/js/eslint.js
@@ -28,7 +28,8 @@ module.exports = (file, text, apply = false) => {
     const options = {
         baseConfig: require(eslintConfig),
         fix: apply,
-        ignore: false,
+        ignore: true,
+        reportUnusedDisableDirectives: true,
     }
     try {
         const engine = new eslint.CLIEngine({
@@ -72,10 +73,16 @@ module.exports = (file, text, apply = false) => {
             if (message.ruleId) {
                 rule = `(${message.ruleId})`
             }
-            response.messages.push({
-                checker: 'eslint',
-                message: `Line ${message.line}: ${message.message} ${rule}`,
-            })
+            if (message.line) {
+                response.messages.push({
+                    checker: 'eslint',
+                    message: `Line ${message.line}: ${message.message} ${rule}`,
+                })
+            } else {
+                log.debug(
+                    `No line number, assuming general message: ${message.message}`
+                )
+            }
         }
     } catch (error) {
         log.error(`ESLint format failed with error:\n${error}`)
