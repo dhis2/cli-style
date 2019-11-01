d42f239bc3323ae5314595208eb5e42bcb199a0b
Viktor Varland <varl@vlv.io>
2019-11-01 09:30:51 +0100

    refactor(cmds): simplify command structure

--- src/cmds/git-commit.js
+++ src/cmds/git-commit.js
@@ -1,10 +1,24 @@
 const { namespace } = require('@dhis2/cli-helpers-engine')
+const log = require('@dhis2/cli-helpers-engine').reporter
+const { commitlint } = require('../tools/commitlint.js')
 
-const command = namespace('commit', {
+const commitCmd = yargs => {
+    return yargs.command(
+        'check',
+        'Checks commit messages according to standards.',
+        function builder(yargs) {
+            return yargs.option('commitlintConfig', {
+                describe: 'Commitlint config file to use',
+                type: 'string',
+            })
+        },
+        function handler(argv) {
+            commitlint(argv.commitlintConfig)
+        }
+    )
+}
+
+module.exports = namespace('commit', {
     describe: 'Format commit messages according to standards.',
-    builder: yargs => {
-        return yargs.commandDir('commit_cmds')
-    },
+    builder: yargs => commitCmd(yargs),
 })
-
-module.exports = command
--- src/cmds/javascript.js
+++ src/cmds/javascript.js
@@ -1,11 +1,90 @@
-const { namespace } = require('@dhis2/cli-helpers-engine')
+const fg = require('fast-glob')
 
-const command = namespace('javascript', {
+const { namespace } = require('@dhis2/cli-helpers-engine')
+const log = require('@dhis2/cli-helpers-engine').reporter
+
+const { eslint } = require('../tools/eslint.js')
+const { prettier } = require('../tools/prettier.js')
+const { blacklist, stagedFiles } = require('../files.js')
+
+const options = yargs =>
+    yargs
+        .option('prettierConfig', {
+            describe: 'Prettier config file to use',
+            type: 'string',
+        })
+        .option('eslintConfig', {
+            describe: 'ESLint config file to use',
+            type: 'string',
+        })
+        .option('pattern', {
+            describe:
+                'Pattern to match for files, remember to enclose in double quotes!',
+            type: 'string',
+            default: '**/*.{js,jsx,ts,tsx}',
+        })
+        .option('staged', {
+            describe: 'Only check staged files in Git',
+            type: 'boolean',
+            default: 'false',
+        })
+
+const handler = (argv, apply) => {
+    const { files, pattern, eslintConfig, prettierConfig, staged } = argv
+
+    const opts = {
+        apply,
+    }
+
+    if (files) {
+        opts.files = files
+    } else {
+        const entries = fg.sync([pattern], {
+            dot: false,
+            ignore: blacklist,
+            absolute: false,
+        })
+
+        opts.files = entries
+    }
+
+    if (staged) {
+        opts.files = stagedFiles(opts.files)
+        log.info('Linting staged files:', opts.files.join(', '))
+    }
+
+    log.info('Running ESLint...')
+    eslint({
+        config: eslintConfig,
+        ...opts,
+    })
+
+    log.info('Running Prettier...')
+    prettier({
+        config: prettierConfig,
+        ...opts,
+    })
+}
+
+const javascriptCmds = yargs => {
+    return yargs
+        .command(
+            'apply',
+            'Apply JS format.',
+            yargs => options(yargs),
+            argv => handler(argv, true)
+        )
+
+        .command(
+            'check',
+            'Check JS format (do not attempt to fix)',
+            yargs => options(yargs),
+            argv => handler(argv, false)
+        )
+}
+
+module.exports = namespace('javascript', {
     aliases: ['js'],
     describe: 'Format javascript according to standards',
-    builder: yargs => {
-        return yargs.commandDir('js_cmds')
-    },
+    builder: yargs => javascriptCmds(yargs),
 })
-
-module.exports = command
