f5930859eaaf2f1b9501916f31cad3c3a57ebabe
Viktor Varland <varl@vlv.io>
2019-12-02 08:36:01 +0100

    fix(husky): load husky config before spreading

---
+++ tests/configs.js
@@ -0,0 +1,35 @@
+const test = require('tape')
+const path = require('path')
+
+const { config } = require('../index.js')
+
+test('prettier config resolves to non-empty object', t => {
+    t.plan(1)
+
+    const loaded = require(config.prettier)
+    t.notDeepEqual(loaded, {})
+})
+
+test('eslint config resolves to non-empty object', t => {
+    t.plan(1)
+
+    const loaded = require(config.eslint)
+    t.notDeepEqual(loaded, {})
+})
+
+test('husky config resolves to non-empty object', t => {
+    t.plan(1)
+
+    const loaded = require(config.husky)
+    t.notDeepEqual(loaded, {})
+})
+
+test('config files reference paths', t => {
+    const count = Object.keys(config).length
+    t.plan(count * 2)
+
+    for (const cfg in config) {
+        t.ok(path.isAbsolute(config[cfg]))
+        t.ok(typeof config[cfg] === 'string')
+    }
+})
