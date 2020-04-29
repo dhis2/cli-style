fd620c56ca8798ab0e84366d97cdbd0d0fdaba03
Viktor Varland <varl@vlv.io>
2020-04-29 10:44:41 +0200

    fix: run the command from inside node_modules
    Allow d2-style to first scan its own node_modules[0] for the deps it
    needs, and if the deps are missing, e.g. if Yarn has hoisted shared deps
    higher up the tree[1], then d2-style can use the standard node module
    resolution behavior to "find-up" its parents node_modules.
    
    Also use find-up to find the nearest .bin folder that has d2-style, and
    use cross-spawn to run the tools instead of npx for consistency.
    
    [0] ./node_modules/@dhis2/cli-style/node_modules/{dep}
    [1] ./node_modules/{dep}

--- src/utils/run.js
+++ src/utils/run.js
@@ -31,6 +31,7 @@ exports.bin = (cmd, { args, opts }, callback) => {
     return handleRun(
         spawn.sync(binCmd, args, {
             stdio: 'inherit',
+            cwd: PACKAGE_ROOT,
             ...opts,
         }),
         callback
