01f69874776fc25ca103ed34cae179030d3d8285
Viktor Varland <viktor@dhis2.org>
2020-10-12 18:18:55 +0200

    refactor: simplify handler

--- src/commands/install.js
+++ src/commands/install.js
@@ -4,6 +4,18 @@ const log = require('@dhis2/cli-helpers-engine').reporter
 const { configure } = require('../utils/config.js')
 const { printGroups, projects } = require('../utils/groups.js')
 
+const promptForProject = async () => {
+    const res = await inquirer.prompt([
+        {
+            name: 'project',
+            message: 'Choose the project template',
+            type: 'list',
+            choices: () => projects.map(a => a[0]),
+        },
+    ])
+    return [`project/${res.project}`]
+}
+
 exports.command = 'install [group..]'
 
 exports.describe = 'Install DHIS2 configurations for a/all group(s)'
@@ -27,28 +39,10 @@ exports.handler = async argv => {
         process.exit(0)
     }
 
-    const prompt = inquirer.createPromptModule()
-
-    const { force, group } = argv
+    const force = argv.force
+    const group = argv.group || (await promptForProject())
 
     const root = process.cwd()
 
-    let choice
-
-    if (!group) {
-        const res = await prompt([
-            {
-                name: 'project',
-                message: 'Choose the project template',
-                type: 'list',
-                choices: () => projects.map(a => a[0]),
-            },
-        ])
-
-        choice = [`project/${res.project}`]
-    } else {
-        choice = group
-    }
-
-    configure(root, choice, force)
+    configure(root, group, force)
 }
