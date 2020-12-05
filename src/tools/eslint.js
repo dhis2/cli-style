288ca40d16a1aedab2928adc8128d66035a6fc98
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-12-05 16:31:29 +0100

    chore: update variable name to indicate package specification

--- src/tools/eslint.js
+++ src/tools/eslint.js
@@ -4,7 +4,7 @@ const { PACKAGE_ROOT } = require('../utils/paths.js')
 
 exports.eslint = ({ files = [], apply = false, config }) => {
     const ignoreFile = resolveIgnoreFile(['.eslintignore'])
-    const cmd = 'eslint'
+    const package = 'eslint'
     const args = [
         '--no-color',
         '--report-unused-disable-directives',
@@ -18,5 +18,5 @@ exports.eslint = ({ files = [], apply = false, config }) => {
         ...files,
     ]
 
-    bin(cmd, { args })
+    bin(package, { args })
 }
