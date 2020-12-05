393041c0580f7d1fa241a235f581c6c570c21903
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-12-05 16:35:14 +0100

    chore: use packageName instead of package (commitlint), don't overload bin variable

--- src/tools/commitlint.js
+++ src/tools/commitlint.js
@@ -2,9 +2,9 @@ const { bin } = require('../utils/run.js')
 const { COMMITLINT_CONFIG } = require('../utils/paths.js')
 
 exports.commitlint = (config = COMMITLINT_CONFIG) => {
-    const package = '@commitlint/cli'
-    const bin = 'commitlint'
+    const packageName = '@commitlint/cli'
+    const cmd = 'commitlint'
     const args = ['commitlint', `--config=${config}`, '--edit']
 
-    bin(package, { bin, args })
+    bin(packageName, { bin: cmd, args })
 }
