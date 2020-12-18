295d2792736642228db8bb20f7d62886461d6c7f
Viktor Varland <viktor@dhis2.org>
2020-12-18 12:24:54 +0100

    feat: commit check can read from a custom file

--- src/commands/commit.js
+++ src/commands/commit.js
@@ -1,16 +1,23 @@
 const log = require('@dhis2/cli-helpers-engine').reporter
 const { namespace } = require('@dhis2/cli-helpers-engine')
 const { commitlint } = require('../tools/commitlint.js')
+const { CONSUMING_ROOT } = require('../utils/paths.js')
 
 const commitCmd = yargs => {
     return yargs.command(
         'check [file]',
-        'Checks commit messages according to standards.',
+        'Checks commit messages according to standard.',
         function builder(yargs) {
-            return yargs.option('commitlintConfig', {
-                describe: 'Commitlint config file to use',
-                type: 'string',
-            })
+            return yargs
+                .positional('file', {
+                    describe: 'File that contains the commit message',
+                    type: 'string',
+                    default: `${CONSUMING_ROOT}/.git/COMMIT_EDITMSG`,
+                })
+                .option('commitlintConfig', {
+                    describe: 'Commitlint config file to use',
+                    type: 'string',
+                })
         },
         function handler(argv) {
             log.info('d2-style > commit')
--- src/tools/commitlint.js
+++ src/tools/commitlint.js
@@ -1,11 +1,11 @@
 const { COMMITLINT_CONFIG } = require('../utils/paths.js')
 const { bin } = require('../utils/run.js')
 
-exports.commitlint = ({ config = COMMITLINT_CONFIG, file }) => {
+exports.commitlint = ({ config = COMMITLINT_CONFIG, file, argv }) => {
     const cmd = 'commitlint'
     const args = [
         'commitlint',
-        `--config=${config}`,
+        ...(config ? ['--config', config] : []),
         ...(file ? ['--edit', file] : []),
     ]
 
