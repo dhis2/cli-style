3ac692947dca9c4afc533404a3af502ffb1e66ef
Viktor Varland <viktor@dhis2.org>
2021-03-12 13:11:28 +0100

    refactor: remove unused argv variable

--- src/tools/commitlint.js
+++ src/tools/commitlint.js
@@ -1,7 +1,7 @@
 const { COMMITLINT_CONFIG } = require('../utils/paths.js')
 const { bin } = require('../utils/run.js')
 
-exports.commitlint = ({ config = COMMITLINT_CONFIG, file, argv }) => {
+exports.commitlint = ({ config = COMMITLINT_CONFIG, file }) => {
     const cmd = 'commitlint'
     const args = [
         'commitlint',
