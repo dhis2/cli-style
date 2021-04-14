89fe772e8937f22c5d572f453660b834d71afc97
Viktor Varland <viktor@dhis2.org>
2021-04-14 14:03:23 +0200

    refactor: store hooks in $PROJECT/.hooks

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -2,7 +2,10 @@ const path = require('path')
 const findup = require('find-up')
 
 const CONSUMING_ROOT = path.join(process.cwd())
+
 const PACKAGE_ROOT = path.join(__dirname, '..', '..')
+const CONFIG_DIR = path.join(PACKAGE_ROOT, 'config')
+const TEMPLATE_DIR = path.join(PACKAGE_ROOT, 'templates')
 
 const PROJECT_ROOT = findup.sync(
     directory => {
@@ -16,12 +19,7 @@ const PROJECT_ROOT = findup.sync(
         type: 'directory',
     }
 )
-
-const CONFIG_DIR = path.join(PACKAGE_ROOT, 'config')
-const TEMPLATE_DIR = path.join(PACKAGE_ROOT, 'templates')
-
-const PROJECT_CONFIG_DIR = path.join(CONSUMING_ROOT, '.d2')
-const PROJECT_HOOKS_DIR = path.join(PROJECT_CONFIG_DIR, 'hooks')
+const PROJECT_HOOKS_DIR = path.join(PROJECT_ROOT, '.hooks')
 
 const STYLE_CONFIG_FILES = [
     'd2-style.config.js',
@@ -35,7 +33,6 @@ module.exports = {
     PACKAGE_ROOT,
     CONFIG_DIR,
     STYLE_CONFIG_FILES,
-    PROJECT_CONFIG_DIR,
     PROJECT_HOOKS_DIR,
     PROJECT_ROOT,
     TEMPLATE_DIR,
