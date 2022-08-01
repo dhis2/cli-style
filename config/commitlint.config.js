1e7cd0ebd9db92021ceaa0faf962570f142a641e
ismay <7355199+ismay@users.noreply.github.com>
2022-08-01 15:57:09 +0200

    fix: ignore skip release commits for commitlint

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    /*
+     * Ignore commits that don't contribute to a release. Release
+     * commits often exceed the max. amount of characters because
+     * of the appended changelog. This ignores those commits.
+     */
+    ignores: [(commit) => commit.includes('[skip release]')],
 }
