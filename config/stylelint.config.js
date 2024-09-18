5ecac76d322c14ef6ca6888021524e9ef0342cad
ismay <git.email@ismay.email>
2024-09-18 10:49:12 +0200

    fix: warn user for unfixable properties

--- config/stylelint.config.js
+++ config/stylelint.config.js
@@ -4,6 +4,27 @@ module.exports = {
     ],
     plugins: ['stylelint-use-logical'],
     rules: {
+        /**
+         * Warn user when properties can't be autofixed:
+         * https://github.com/csstools/stylelint-use-logical/issues/29
+         */
+        'declaration-property-value-disallowed-list': [
+            {
+                'border-color': [],
+                'border-style': [],
+                'border-width': [],
+                inset: [],
+                margin: [],
+                padding: [],
+                'scroll-margin': [],
+                'scroll-padding': [],
+            },
+            {
+                message: (prop) =>
+                    `Use longhand form of ${prop} so that it can be autofixed to logical properties and values`,
+                severity: 'warning',
+            },
+        ],
         'csstools/use-logical': [
             true,
             {
