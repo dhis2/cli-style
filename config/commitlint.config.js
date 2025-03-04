80483d264d5bc547f38340b801ea839b3422ce26
Mozafar <mozafar@dhis2.org>
2025-03-04 16:23:26 +0000

    fix: update commitlint.config header max length

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 120],
+    },
     /*
      * Ignore commits that don't contribute to a release. Release
      * commits often exceed the max. amount of characters because
