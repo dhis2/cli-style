d60482d416b9c70a82d031df735e52a06e821ae6
Mozafar <mozafar@dhis2.org>
2025-04-03 11:08:02 +0100

    fix: update commitlint.config header max length (#478)

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
+        'header-max-length': [2, 'always', 120],
         'body-max-line-length': [1, 'always', 100],
     },
     /*
